c84b21a5f4f93b16f092ed55cdef9b490f6f4939
zhangsan <zhangsan@itcast.cn>
2018-11-28 10:28:15 +0800

    first

---
+++ git/src/cn/itcast/git/GitTest1.java
@@ -0,0 +1,11 @@
+package cn.itcast.git;
+
+public class GitTest1 {
+
+    public static void main(String[] args) {
+        System.out.println("1111111111");
+        System.out.println("1111111111");
+        System.out.println("1111111111");
+        System.out.println("1111111111");
+    }
+}
